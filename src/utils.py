22008f7f0fa089ff26f9391136a0cbe6ba87cb13
valeger <gerasimenkovaleriya.i@gmail.com>
2020-09-19 14:13:47 +0300

    add train and eval functions

--- src/utils.py
+++ src/utils.py
@@ -56,3 +56,68 @@ class ConvEncoder(nn.Module):
         mu, log_std = self.fc(out).chunk(2, dim=1)
         return mu, log_std
 
+
+def train(model: nn.Module, 
+          train_loader: DataLoader, 
+          optimizer: optim.Adam, 
+          epoch: int,
+          device: torch.device,
+          grad_clip=None) -> Dict:         
+    model.train()
+
+    losses = OrderedDict()
+    for x in train_loader:
+        x = x.to(device)
+        out = model.loss(x)
+        optimizer.zero_grad()
+        out['loss'].backward()
+        if grad_clip:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), grad_clip)
+        optimizer.step()
+
+        for k, v in out.items():
+            if k not in losses:
+                losses[k] = []
+            losses[k].append(v.item())
+    return losses
+
+
+def eval_loss(model: nn.Module, data_loader: DataLoader, device: torch.device) -> Dict:
+    model.eval()
+    total_losses = OrderedDict()
+    with torch.no_grad():
+        for x in data_loader:
+            x = x.to(device)
+            out = model.loss(x)
+            for k, v in out.items():
+                total_losses[k] = total_losses.get(k, 0) + v.item() * x.shape[0]
+
+        for k in total_losses.keys():
+            total_losses[k] /= len(data_loader.dataset)
+    return total_losses
+
+
+def train_epochs(model: nn.Module,
+                 train_loader: DataLoader, 
+                 test_loader: DataLoader, 
+                 params: Dict) -> Tuple[Dict]:
+    epochs, lr, device = params['epochs'], params['lr'], params['device']
+    grad_clip = params.get('grad_clip', None)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    train_losses, test_losses = OrderedDict(), OrderedDict()
+    for epoch in range(epochs):
+        model.train()
+        train_loss = train(model, train_loader, optimizer, epoch, device, grad_clip)
+        test_loss = eval_loss(model, test_loader, device)
+
+        for k in train_loss.keys():
+            if k not in train_losses:
+                train_losses[k] = []
+                test_losses[k] = []
+            train_losses[k].extend(train_loss[k])
+            test_losses[k].append(test_loss[k])
+            print(f'Epoch {epoch}: train {k}: {np.mean(train_loss[k]):.4f}\n')
+            print(f'Epoch {epoch}: test {k}: {test_loss[k]:.4f}\n')
+        print('--------------------------------------------------')
+    return train_losses, test_losses
